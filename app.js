ebd1fbba45cf5fedc4d5e57ad05ade1e65f41e78
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-07-07 21:28:09 +0300

    loop through images array and provide 30 images

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 const images = ['images/img1.jpg','images/img2.jpg','images/img3.jpg','images/img4.jpg','images/img5.jpg'];
 
+const img = document.querySelector('.img');
+
+// Loop through images array and provide 30 images
+for(let i = 0;i < 30; i++){
+    let imgSrc = images[i%images.length]
+ img.innerHTML +=`<img src=${imgSrc}  alt='' />`;
+}
